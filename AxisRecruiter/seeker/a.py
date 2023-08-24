c0b9bce584cdc3f14c0515847522bfef648bc9c0
Jagruti Ravindra Patil <103519235+JagrutiPatil9730@users.noreply.github.com>
2023-08-24 16:44:30 +0530

    Delete AxisRecruiter directory

--- AxisRecruiter/seeker/a.py
+++
@@ -1,9 +0,0 @@
-import glob
-import wget;
-
-url='https://docs.intersystems.com/irisforhealth20231/csp/docbook/pdfs.zip';
-wget.download(url)
-# extract docs
-import zipfile
-with zipfile.ZipFile('pdfs.zip','r') as zip_ref:
-  zip_ref.extractall('.')
